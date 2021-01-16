e4fcdab27c57e7cae9cc06d9134d63caa8575f4a
keisuke-kiriyama <krkr.xm.rkr7x@gmail.com>
2021-01-16 12:20:20 +0900

    add index.html

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ func main() {
 
 	// handle static assets
 	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir(config.Static))
-	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	// TODO: bootstrapでなんかやるっぽい。後で
+	// files := http.FileServer(http.Dir(config.Static))
+	// mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	//
+	// all route patterns matched here
+	// route handler functions defined in other files
+	//
+
+	// index
+	mux.HandleFunc("/", index)
+	// error
+	mux.HandleFunc("/err", err)
+
 }
